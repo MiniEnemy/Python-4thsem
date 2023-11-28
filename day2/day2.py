90272744234b452fb52561ea9a8bad8f91b63a28
MiniEnemy <home.minj@gmail.com>
2023-11-28 12:16:30 +0545

    added old files to new

---
+++ day2/day2.py
@@ -0,0 +1,14 @@
+# print("Hello world")
+# print('hello','world')
+# print('hello','world', sep='')
+# print("hello" "world",)
+a=6
+b=10
+print(f"THe number is {a}",f"second number is {b}")
+c=a+b
+print("sum is",a+b)
+print(type(a))
+b=int (5.7)
+c=float(7.9)
+power=5^6
+print(b)
