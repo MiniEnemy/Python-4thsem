f33d7d9797da57925572480fc3ce6322c577d6f0
MiniEnemy <home.minj@gmail.com>
2023-12-03 12:13:20 +0545

    some comments

---
+++ day3/day3.py
@@ -0,0 +1,15 @@
+num_1=5
+print(type(num_1))
+
+# Variable Naming convention
+# Variable name only contains number , underscore,character
+# Special character not allowed.
+# Space not allowed
+# Can't Start with numbers
+# Can't use reserved keywords like if,else,elif,break,continue,while,for
+
+#data types
+
+#Nunerii data tyes
+#sequence data tyeps
+
